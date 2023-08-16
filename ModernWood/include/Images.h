24d8f2c5bcdeb9a18e0c3383a24acd686f53dc2a
Carlos Lopez Martinez <69404571+Electroner@users.noreply.github.com>
2023-08-16 13:35:47 +0200

    Changed value of configuration parameters submenu

--- ModernWood/include/Images.h
+++ ModernWood/include/Images.h
@@ -633,6 +633,6 @@ const struct General_Screen_display{
 
 //The max size on the option value is 36 pixels
 const struct Option_Value_Position_display{
-    const uint8_t x = 120;
+    const uint8_t x = 125;
     const uint8_t y = 26;
 }Option_Value_Position_display;
